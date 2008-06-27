e5f161abf0a5250212417a18d96e58d36d24b5d0
Walery Studennikov <despair@cpan.org>
2008-06-27 09:20:47 +0000

    Import of DESPAIR/MySQL-Insert-0.02 from CPAN.
    gitpan-cpan-distribution: MySQL-Insert
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DESPAIR/MySQL-Insert-0.02.tar.gz
    gitpan-cpan-author:       DESPAIR
    gitpan-cpan-maturity:     released

--- lib/MySQL/Insert.pm
+++ lib/MySQL/Insert.pm
@@ -11,27 +11,30 @@ MySQL::Insert - extended inserts for MySQL via DBI
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+    # Insert two rows into sample_table using $dbh database handle
 
     use MySQL::Insert;
 
     $MySQL::Insert::MAX_ROWS_TO_QUERY = 1000;
 
-    my $inserter = MySQL::Insert->new();
+    my $inserter = MySQL::Insert->new( $dbh, 'sample_table' );
 
-    $inserter->insert_row( { fldname => 'fldvalue' } );
+    $inserter->insert_row( { fldname => 'fldvalue1' } );
+    $inserter->insert_row( { fldname => 'fldvalue2' } );
 
     undef $inserter;
 
-=head1 FUNCTIONS
+=head1 FUNCTIONS / METHODS
+
+The following methods are available:
 
 =head2 new
+
+Create new MySQL::Insert object
 
 =cut
 
@@ -44,25 +47,15 @@ sub new {
     return $self;
 }
 
-sub table {
-    my ($self, $table) = @_;
-
-    if ($table) {
-	$self->{_table} = $table;
-    }
-
-    return $self->{_table};
-}
-
 sub _init {
     my $self = shift;
     my $dbh = shift;
     my $table = shift;
-    my $field = shift;
+    my $fields = shift;
 
     $self->{_dbh} = $dbh;
-    $self->{_fields} = $field;
-    $self->{_name_fields} = join ", ", @$field;
+    $self->{_fields} = $fields;
+    $self->{_name_fields} = join ", ", @$fields;
     $self->{_table} = $table;
     $self->{_finalize_row} = 1;
     $self->{_query_exists} = 0;
@@ -71,21 +64,23 @@ sub _init {
 DESTROY {
     my $self = shift;
 
-    $self->finish_row;
+    $self->_finish_row;
 
     if ($self->{_query_exists}) {
-	$self->execute_query();
+	$self->_execute_query();
     }
 }
 
 =head2 insert_row
+
+Schedule row for insertion
 
 =cut
 
 sub insert_row {
     my ($self, $new_row) = @_;
 
-    my $query_executed = $self->finish_row();
+    my $query_executed = $self->_finish_row();
 
     $self->{_finalize_row} = 0;
     $self->{_row} = $new_row;
@@ -93,17 +88,19 @@ sub insert_row {
     return $query_executed;
 }
 
-sub finish_row {
+# Private methods
+
+sub _finish_row {
     my $self = shift;
 
     my $query_executed;
 
     if (!$self->{_finalize_row}) {
 	if ($self->{_query_exists} && $self->{_total_rows} >= $MAX_ROWS_TO_QUERY) {
-	    $query_executed = $self->execute_query();
+	    $query_executed = $self->_execute_query();
 	}
 
-	$self->print_row;
+	$self->_print_row;
 
 	$self->{_finalize_row} = 1;
     }
@@ -111,7 +108,7 @@ sub finish_row {
     return $query_executed;
 }
 
-sub execute_query {
+sub _execute_query {
     my $self = shift;
 
     my $query = qq|INSERT IGNORE $self->{_table} ($self->{_name_fields}) VALUES |
@@ -126,7 +123,7 @@ sub execute_query {
     return $result;
 }
 
-sub print_row {
+sub _print_row {
     my $self = shift;
 
     my @data_row = ();
@@ -152,43 +149,9 @@ Please report any bugs or feature requests to C<bug-mysql-insert at rt.cpan.org>
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MySQL-Insert>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc MySQL::Insert
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MySQL-Insert>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MySQL-Insert>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MySQL-Insert>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MySQL-Insert>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Walery Studennikov, all rights reserved.
+Copyright 2008 Gleb Tumanov (gleb at reg.ru), all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
